a10170f56604c572f1b317ae9b2b3d37173eb5da
yoyo <kamahat@hotmail.com>
2020-03-05 01:21:24 +0100

    Test Config Neopixel

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2217,7 +2217,7 @@
 
   // Use a single Neopixel LED for static (background) lighting
   //#define NEOPIXEL_BKGD_LED_INDEX  0               // Index of the LED to use
-  //#define NEOPIXEL_BKGD_COLOR { 255, 255, 255, 0 } // R, G, B, W
+  #define NEOPIXEL_BKGD_COLOR { 255, 255, 255, 64 } // R, G, B, W
 #endif
 
 /**
