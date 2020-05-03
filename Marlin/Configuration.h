4efe748a82555b2ae35db03da67ec8d1e35a07cd
yoyo <kamahat@hotmail.com>
2020-05-03 14:48:35 +0200

    Limite bed avec miroir

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -978,10 +978,11 @@
  * Specify a Probe position as { X, Y, Z }
  */
 #define NOZZLE_TO_PROBE_OFFSET { 0, 70, 10 }
+#define ProbeDiameter 12
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define MIN_PROBE_EDGE 5
+#define MIN_PROBE_EDGE 2 + ProbeDiameter/2
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 7200
@@ -1113,15 +1114,15 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 328
-#define Y_BED_SIZE 340
+#define X_BED_SIZE 300
+#define Y_BED_SIZE 300
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -3
-#define Y_MIN_POS -21
+#define X_MIN_POS -13
+#define Y_MIN_POS -27
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE + X_MIN_POS
-#define Y_MAX_POS Y_BED_SIZE + Y_MIN_POS
+#define X_MAX_POS 328 + X_MIN_POS
+#define Y_MAX_POS 340 + Y_MIN_POS
 #define Z_MAX_POS 307 + Z_MIN_POS
 
 /**
